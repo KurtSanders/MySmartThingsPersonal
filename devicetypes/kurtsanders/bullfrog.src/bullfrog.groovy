5772d1f7bc3dff8d8c087acfa2357962e7ee48f8
Kurt Sanders <KurtSanders@users.noreply.github.com>
2017-03-15 23:42:04 -0400

    Added Subrotuines

--- devicetypes/kurtsanders/bullfrog.src/bullfrog.groovy
+++ devicetypes/kurtsanders/bullfrog.src/bullfrog.groovy
@@ -13,8 +13,7 @@
  *  for the specific language governing permissions and limitations under the License.
  *
  */
- 
-def devID_old = "00000000-00000000-001527FF-FF09818B"
+import java.text.SimpleDateFormat; 
 
 metadata {
 	definition (name: "bullfrog", namespace: "kurtsanders", author: "kurt@kurtsanders.com") {
@@ -35,12 +34,12 @@ metadata {
 				attributeState "turningOff", label:'${name}', action:"switch.on", icon:"st.switches.switch.off", backgroundColor:"#ffff00", nextState:"turningOn"
 			}
  			tileAttribute("device.statusText", key: "SECONDARY_CONTROL") {
-				attributeState "statusText", label: 'Spa Error: Device Not Connected', backgroundColor:"#79b821", defaultState: true
+				attributeState "statusText", label: '${currentValue}', backgroundColor:"#79b821", defaultState: true
 			}         
 		}
 
-        valueTile("spaTemp", "device.spaTemp", width: 2, height: 2) {
-            state("spaTemp", label:'${currentValue}°F',
+        valueTile("spaCurTemp", "device.spaCurTemp", width: 2, height: 2) {
+            state("spaCurTemp", label:'${currentValue}°F',
                 backgroundColors:[
                     [value: 50,  color: "#153591"],
                     [value: 98,  color: "#1e9cbb"],
@@ -52,41 +51,44 @@ metadata {
                 ]
             )
         }
+        valueTile("spaSetTemp", "device.spaSetTemp", width: 2, height: 2) {
+            state("spaSetTemp", label:'${currentValue}')
+        }
  		standardTile("spaPump1", "device.spaPump1", inactiveLabel: false,
 			decoration: "flat", width: 2, height: 2,) {
-			state "On", label: '${name}', 
+			state "On", label: '${currentValue}', 
 				icon: "st.valves.water.open", backgroundColor: "#FF0000"
-			state "Off", label: '${name}', 
+			state "Off", label: '${currentValue}', 
 				icon: "st.valves.water.closed", backgroundColor: "#ffffff"
 		}
  		standardTile("spaPump2", "device.spaPump2", inactiveLabel: false,
 			decoration: "flat", width: 2, height: 2,) {
-			state "On", label: '${name}', 
+			state "On", label: '${currentValue}', 
 				icon: "st.valves.water.open", backgroundColor: "#FF0000"
-			state "Off", label: '${name}', 
+			state "Off", label: '${currentValue}', 
 				icon: "st.valves.water.closed", backgroundColor: "#ffffff"
 		}
  		standardTile("ledLights", "device.ledLights", inactiveLabel: false,
 			decoration: "flat", width: 2, height: 2,) {
-			state "On", label: '${name}', 
+			state "On", label: '${currentValue}', 
 				icon: "st.Lighting.light11", backgroundColor: "#FF0000"
-			state "Off", label: '${name}', 
+			state "Off", label: '${currentValue}', 
 				icon: "st.Lighting.light11", backgroundColor: "#ffffff"
 		}
  		standardTile("modeState", "device.modeState", inactiveLabel: false,
 			decoration: "flat", width: 2, height: 2,) {
-			state "Rest", label: '${name}', 
+			state "Rest", label: '${currentValue}', 
 				icon: "st.thermostat.fan-circulate", backgroundColor: "#C1C2C3"
-			state "Ready", label: '${name}', 
+			state "Ready", label: '${currentValue}', 
 				icon: "st.thermostat.fan-circulate", backgroundColor: "#008000"
-			state "Off", label: '${name}', 
+			state "Off", label: '${currentValue}', 
 				icon: "st.thermostat.fan-circulate", backgroundColor: "#0000FF"
 		}
  		standardTile("heatMode", "device.heatMode", inactiveLabel: false,
 			decoration: "flat", width: 2, height: 2,) {
-			state "On", label: '${name}', 
+			state "On", label: '${currentValue}', 
 				icon: "st.thermostat.heat", backgroundColor: "#FF0000"
-			state "Off", label: '${name}', 
+			state "Off", label: '${currentValue}', 
 				icon: "st.thermostat.heat", backgroundColor: "#ffffff"
 		}
         standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 6, height: 2) {
@@ -94,21 +96,12 @@ metadata {
         }
         
 		main(["main"])
-		details(["main", "spaTemp", "spaPump1", "spaPump2", "ledLights", "modeState", "heatMode", "refresh"])
+		details(["main", "spaSetTemp", "spaCurTemp", "modeState", "heatMode", "spaPump1", "spaPump2", "ledLights", "refresh"])
 	}
 }
 
 def installed() {
 	log.debug "Installed: Begin..."
-    /*
-	sendEvent(name: "device.spaTemp"   , value: 100)
-	sendEvent(name: "device.statusText", value: "Connected.")
-	sendEvent(name: "device.spaPump1"	, value: "Off")
-	sendEvent(name: "device.spaPump2"	, value: "Off")
-	sendEvent(name: "device.ledLights"	, value: "Off")
-	sendEvent(name: "device.modeState"	, value: "Ready")
-    sendEvent(name: "device.heatMode"	, value: "On")
-    */
 	log.debug "Installed: End..."
 }
 
@@ -118,111 +111,191 @@ def parse(String description) {
 
 def setMode(state) {
 	log.debug "setMode(${state}): Entering"
-
 }
-
 
 def on() {
 	log.debug "turningOn"
 	sendEvent(name: "switch", value: "on")
-    getStatus()
+    updateBullfrogStatus()
 }
 
 def off() {
 	log.debug "turningOff"
 	sendEvent(name: "switch", value: "off")
-    getStatus()
+    updateBullfrogStatus()
+    return
 }
 
 def refresh() {
     log.trace("--- handler.refresh")
-    getStatus()
+    updateBullfrogStatus()
 	return
 }
 
-def getStatus() {
-	log.debug "getStatus(): Begin"
-    def devID = " "
-    def url   = "https://my.idigi.com/ws/DeviceCore/.json?condition=dpGlobalIp='174.101.169.159'"
+def updateBullfrogStatus() {
+    def devID 	= "00000000-00000000-001527FF-FF09818B"
+    def ip		= "174.101.169.159"
     def header = [
-    'UserAgent': 'Spa / 48 CFNetwork / 758.5.3 Darwin / 15.6.0', 
-    'Cookie': 'JSESSIONID = BC58572FF42D65B183B0318CF3B69470; BIGipServerAWS - DC - CC - Pool - 80 = 3959758764.20480.0000', 
-    'Authorization': 'Basic QmFsYm9hV2F0ZXJJT1NBcHA6azJuVXBSOHIh'
+        'UserAgent': 'Spa / 48 CFNetwork / 758.5.3 Darwin / 15.6.0', 
+        'Cookie': 'JSESSIONID = BC58572FF42D65B183B0318CF3B69470; BIGipServerAWS - DC - CC - Pool - 80 = 3959758764.20480.0000', 
+        'Authorization': 'Basic QmFsYm9hV2F0ZXJJT1NBcHA6azJuVXBSOHIh'
     ]
+
+    def skip_getDevId = true
+
+    if (skip_getDevId==false) {
+        devID = getDevId(ip, header)
+        if (devID == false) {
+            return
+        }
+    }
+
+	def byte[] B64decoded = getOnlineData(devID, header)
+    log.debug "B64decoded from getOnlineData(): ${B64decoded}"
+    
+	if (B64decoded==false) {
+		return
+    }
+	decodeBullfrogB64Data(B64decoded)
+}
+
+def getDevId(p,h) {
+    log.debug "getOnlineStatus(): Begin"
+    def devID = ""
+    def ip 		= p
+    def header	= h
+    def url   	= "https://my.idigi.com/ws/DeviceCore/.json?condition=dpGlobalIp='" + ip + "'"
     def params = [
         'uri'			: url,
         'headers'		: header,
         'contentType'	: 'application/json'
     ]
-        log.debug "Start httpGet ============="
-        try {
-            httpGet(params) 
-            { resp -> 
-//            log.info "response data: ${resp.data}"
-        	devID = resp.data.items.devConnectwareId?.get(0)
+    log.debug "Start httpGet ============="
+    try {
+        httpGet(params) 
+        { resp -> 
+            // log.debug "response data: ${resp.data}"
+            devID = resp.data.items.devConnectwareId?.get(0)
             log.info "devID = ${devID}"
-			if(resp.status == 200) {
+            if(resp.status == 200) {
                 log.debug "HttpGet Request was OK"
-                }
+            }
             else {
                 log.error "HttpGet Request got http status ${resp.status}"
-                return 
-        		}
+                return false 
             }
-        }    
-        catch (Exception e) 
-        {
-            log.debug e
         }
+    }    
+    catch (Exception e) 
+    {
+        log.debug e
+        return false
+    }
+    return devID
+}
 
-		log.debug "Web_postdata: Start"
-        def Web_idigi_post  = "https://developer.idigi.com/ws/sci"
-        def Web_postdata 	= '<sci_request version="1.0"><file_system cache="false" syncTimeout="15">\
-        <targets><device id="' + "${devID}" + '"/></targets><commands><get_file path="PanelUpdate.txt"/>\
-        <get_file path="DeviceConfiguration.txt"/></commands></file_system></sci_request>'
-        
-        params = [
-            'uri'			: Web_idigi_post,
-            'headers'		: header,
-			'body'			: Web_postdata	
-			]
-            log.debug "Start httpGet ============="
-            try {
-                httpPost(params) 
-                { resp -> 
-				if(resp.status == 200) {
-                    log.debug "HttpGet Request was OK"
-                    sendEvent(name: "device.statusText", value: "Connected.")
-                    log.info "response data: ${resp.data}"
-                    def B64encoded = resp.data
-                    def byte[] B64decoded = B64encoded.decodeBase64()
-                    log.info "decoded: ${B64decoded}"
-                    def hexstring = B64decoded.encodeHex()
-                    log.info "hexstring: ${hexstring}"
-					def setTemp 	= B64decoded.getAt(24)
-					def ledState 	= B64decoded.getAt(18)
-                    if (B64decoded[18]>0) { 
-                    	log.info "LED On"
-                    }
-                    else {
-                    	log.info "LED Off"
-                    }
-					log.debug "setTemp: ${setTemp}"
-					log.debug "ledState: ${ledState}"
-//                    log.info "Set Temp: ${hexstring.get(24)}"
-                    }
-                else {
-                    log.error "HttpGet Request got http status ${resp.status}"
-                    sendEvent(name: "device.statusText", value: "Spa Error: Device Not Connected.")
-                    return 
-                    }
-                }
-            }    
-            catch (Exception e) 
-            {
-                log.debug e
+def byte[] getOnlineData(d, h) {
+    log.debug "getOnlineData: Start"
+    def devID 		= d
+    def header 		= h
+    def byte[] B64decoded
+    Date now = new Date()
+    def timeString = now.format("EEE MM/dd hh:mm:ss a", location.timeZone)
+    log.debug "timeString: ${timeString}"
+
+    def Web_idigi_post  = "https://developer.idigi.com/ws/sci"
+    def Web_postdata 	= '<sci_request version="1.0"><file_system cache="false" syncTimeout="15">\
+    <targets><device id="' + "${devID}" + '"/></targets><commands><get_file path="PanelUpdate.txt"/>\
+    <get_file path="DeviceConfiguration.txt"/></commands></file_system></sci_request>'
+    //    log.debug "Web_postdata: ${Web_postdata}"
+
+    def params = [
+        'uri'			: Web_idigi_post,
+        'headers'		: header,
+        'body'			: Web_postdata	
+    ]
+    log.debug "Start httpPost ============="
+    try {
+        httpPost(params) 
+        { resp -> 
+            if(resp.status == 200) {
+                log.debug "HttpPost Request was OK"
+                sendEvent(name: "statusText", value: "Bullfrog is Online at ${timeString}!")
+                //                log.info "response data: ${resp.data}"
+                def B64encoded = resp.data
+//                def byte[] B64decoded = B64encoded.decodeBase64()
+                B64decoded = B64encoded.decodeBase64()
+                log.info "B64decoded: ${B64decoded}"
+                //                    def hexstring = B64decoded.encodeHex()
+                //                    log.info "hexstring: ${hexstring}"
             }
-        log.debug "getStatus(): End"
-        
-	}
-    
+            else {
+                log.error "HttpGet Request got http status ${resp.status}"
+                sendEvent(name: "statusText", value: "Spa Error: Device Not Connected at ${timeString}.")
+                return False
+            }
+        }
+    }    
+    catch (Exception e) 
+    {
+        log.debug e
+        return False
+    }
+    log.debug "getOnlineData: End-> ${B64decoded}"
+    return B64decoded
+}
+
+def decodeBullfrogB64Data(byte[] d) {
+	log.debug "Entering decodeBullfrogB64Data(${d})"
+	def byte[] B64decoded = d
+
+    def offset = 9
+    switch (B64decoded[offset]) {
+        case 0:
+        log.info "Mode: Ready"
+        sendEvent(name: "modeState", value: 'Ready')
+        break
+        case 1:
+        log.info "Mode: Rest"
+        sendEvent(name: "modeState", value: 'Rest')
+        break
+        case 2:
+        log.info "Mode: Ready in Rest"
+        sendEvent(name: "modeState", value: 'Ready in Rest')
+        break
+        default :
+        log.info "Mode: Unknown"
+        sendEvent(name: "modeState", value: 'Unknown')					                        
+    }
+
+    offset = 12
+    log.debug "setCurTemp: ${B64decoded[offset]}"
+    sendEvent(name: "spaCurTemp", value: B64decoded[offset])
+
+    offset = 17
+    log.debug "heatMode: ${B64decoded[offset]}"
+    if (B64decoded[offset]>0) { 
+        log.info "Heat On"
+        sendEvent(name: "heatMode", value: 'On')
+    }
+    else {
+        log.info "Heat Off"
+        sendEvent(name: "heatMode", value: 'Off')
+    }
+
+    offset = 18
+    log.debug "ledState: ${B64decoded[offset]}"
+    if (B64decoded[offset]>0) { 
+        log.info "LED On"
+        sendEvent(name: "ledLights", value: 'On')
+    }
+    else {
+        log.info "LED Off"
+        sendEvent(name: "ledLights", value: 'Off')
+    }
+
+    offset = 24
+    log.debug "setSetTemp: ${B64decoded[offset]}"
+    sendEvent(name: "spaSetTemp", value: B64decoded[offset] + '°F\nSet Mode')
+
+}
